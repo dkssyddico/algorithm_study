27b3ca337503e9230ba291c7f0b867bc97ba190b
dkssyddico <dkssyddico@naver.com>
2021-06-12 01:18:39 +0900

    change to node.js system and [210611] inflearn complete

---
+++ inflearn/basic/minNum.js
@@ -0,0 +1,26 @@
+// My solution
+// let numbers = [5, 3, 7, 11, 1, 15, 17];
+
+// function solution(array) {
+//   let answer;
+//   array.sort((a, b) => a - b);
+//   answer = array[0];
+//   return answer;
+// }
+
+// console.log(solution(numbers));
+
+// Teacher's solution
+function solution2(array) {
+  let answer,
+    min = Number.MAX_SAFE_INTEGER;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) min = array[i];
+  }
+  answer = min;
+  return answer;
+}
+
+let result = solution2(numbers);
+
+console.log('');
