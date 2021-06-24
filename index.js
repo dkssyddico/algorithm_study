12d002b6166771cf736d272dd599fb01c1c41d6d
dkssyddico <dkssyddico@naver.com>
2021-06-24 20:30:27 +0900

    [210623] inflearn complete

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 function solution(arr) {
   let answer = [];
-  answer.push(arr[0]);
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > arr[i - 1]) answer.push(arr[i]);
+  let max = arr[0];
+  answer.push(max);
+  for (let x of arr) {
+    if (x > max) {
+      max = x;
+      answer.push(x);
+    }
   }
+  return answer.length;
+}
 
+let arr = [130, 135, 148, 140, 145, 150, 150, 153];
+
+console.log(solution(arr));
+
+function tSolution(arr) {
+  let answer = 1,
+    max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      answer++;
+      max = arr[i];
+    }
+  }
   return answer;
 }
 
-let arr = [7, 3, 9, 5, 6, 12];
-
-console.log(solution(arr));
+console.log(tSolution(arr));
