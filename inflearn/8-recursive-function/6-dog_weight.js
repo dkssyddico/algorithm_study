8511fd1137125f93e30267c2e023cfd3abed3d2c
dkssyddico <dkssyddico@naver.com>
2021-08-10 11:03:43 +0900

    [210810] inflearn complete

--- inflearn/8-recursive-function/6-dog_weight.js
+++ inflearn/8-recursive-function/6-dog_weight.js
@@ -32,3 +32,21 @@ function solution(n, arr) {
 let arr = [81, 58, 42, 33, 61];
 
 console.log(solution(259, arr));
+
+function tSolution(c, arr) {
+  let answer = Number.MIN_SAFE_INTEGER;
+  let n = arr.length;
+  function DFS(L, sum) {
+    if (sum > c) return;
+    if (L === n) {
+      answer = Math.max(answer, sum);
+    } else {
+      DFS(L + 1, sum + arr[L]);
+      DFS(L + 1, sum);
+    }
+  }
+  DFS(0, 0);
+  return answer;
+}
+
+console.log(tSolution(259, arr));
