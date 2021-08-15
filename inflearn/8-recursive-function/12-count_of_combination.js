a8982b38e94cef6a79128e54f8ac99e0516111a0
dkssyddico <dkssyddico@naver.com>
2021-08-15 12:52:48 +0900

    [210815] inflearn complete

---
+++ inflearn/8-recursive-function/12-count_of_combination.js
@@ -0,0 +1,16 @@
+// 조합의 경우수
+
+function solution(n, r) {
+  let answer;
+  // 문제 내
+  let dy = Array.from(Array(n + 1), () => Array(n + 1).fill(0));
+  function DFS(n, r) {
+    if (dy[n][r] > 0) return dy[n][r];
+    if (n === r || r === 0) return 1;
+    else return (dy[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r));
+  }
+  answer = DFS(n, r);
+  return answer;
+}
+
+console.log(solution(33, 19));
