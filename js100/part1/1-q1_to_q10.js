ea2abb291038f83640279ff03fd672476dc40bff
dkssyddico <dkssyddico@naver.com>
2021-10-10 18:02:49 +0900

    [211010] programmers complete

--- js100/part1/1-q1_to_q10.js
+++ js100/part1/1-q1_to_q10.js
@@ -117,18 +117,7 @@ var second = '27';
 // var result = `${year}/${month}/${day} ${hour}:${minute}:${second}`;
 
 //concat() 메서드는 매개변수로 전달된 문자열을 메서드를 호출한 문자열에 붙여 새로운 문자열로 반환합니다.
-var result = year.concat(
-  '/',
-  month,
-  '/',
-  day,
-  ' ',
-  hour,
-  ':',
-  minute,
-  ':',
-  second
-);
+var result = year.concat('/', month, '/', day, ' ', hour, ':', minute, ':', second);
 
 console.log(result);
 
