8b063d6892b7abf5acb02306f62560f7eda3f0dd
dkssyddico <dkssyddico@naver.com>
2021-10-22 22:23:49 +0900

    [211022] BAEKJOON complete

--- BAEKJOON/11-brute_force.js
+++ BAEKJOON/11-brute_force.js
@@ -102,3 +102,88 @@
 
   console.log(ranks.join(' '));
 }
+
+{
+  // 1018번 체스판 다시 칠하기
+  const fs = require('fs');
+  const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+  const [N, M] = input
+    .shift()
+    .split(' ')
+    .map((mn) => parseInt(mn));
+
+  const board = [];
+  input.forEach((line) => {
+    board.push(line);
+  });
+
+  let column = N;
+  let row = M;
+
+  const whiteList = [
+    'WBWBWBWB',
+    'BWBWBWBW',
+    'WBWBWBWB',
+    'BWBWBWBW',
+    'WBWBWBWB',
+    'BWBWBWBW',
+    'WBWBWBWB',
+    'BWBWBWBW',
+  ];
+
+  const blackList = [
+    'BWBWBWBW',
+    'WBWBWBWB',
+    'BWBWBWBW',
+    'WBWBWBWB',
+    'BWBWBWBW',
+    'WBWBWBWB',
+    'BWBWBWBW',
+    'WBWBWBWB',
+  ];
+
+  let wrongCounts = [];
+
+  // 체스판 구하기: 체스판을 0, 0부터 시작해서 옆으로 그리고 아래로 한 블럭씩 이동시키는 것
+  for (let i = 0; i + 7 < column; i++) {
+    for (let j = 0; j + 7 < row; j++) {
+      wrongCounts.push(compareBoards(i, j, board, whiteList));
+      wrongCounts.push(compareBoards(i, j, board, blackList));
+    }
+  }
+
+  function compareBoards(y, x, board, compareBoard) {
+    let count = 0;
+    // 한 줄씩 쭈르르 비교
+    for (let i = y; i < y + 8; i++) {
+      for (let j = x; j < x + 8; j++) {
+        if (board[i][j] !== compareBoard[i - y][j - x]) {
+          count++;
+        }
+      }
+    }
+    return count;
+  }
+
+  console.log(Math.min(...wrongCounts));
+}
+
+{
+  /**
+   * 1436번
+   * 최대 숫자까지 for문 돌려서 666들어간 수 다 구해서 array에 넣기
+   * 처음에 생각하는건 666이 들어간 숫자만 있는 배열을 만들고 거기서 index를 찾으려고 했는데, 666이 나올 수 있는 숫자 제한이 없고 n번째에 대한 제한만 나와서 실패. 그래도 숫자를 문자열로 만들어서 includes든 match를 하려고 하든 생각해낸건 잘한듯!
+   */
+
+  let input = Number(require('fs').readFileSync('/dev/stdin').toString().trim());
+  let count = 1;
+  let num = 666;
+  while (input !== count) {
+    num++;
+    if (String(num).includes('666')) {
+      count++;
+    }
+  }
+
+  console.log(num);
+}
