beef881db6c49da01b54ec3448c8a002d5858527
dkssyddico <dkssyddico@naver.com>
2021-12-03 11:53:09 +0900

    [211203] BOJ incomplete

--- BAEKJOON/16-greedy.js
+++ BAEKJOON/16-greedy.js
@@ -419,3 +419,118 @@
 
   console.log(m[0] + m[1], M[0] + M[1]);
 }
+
+{
+  /**
+   * 1715번 카드 정렬하기
+   * 어려운 문제.
+   * 어떻게 풀어야 하는지는 도출해냈다.
+   * 카드를 오름차순으로 정렬하고, 2개의 카드를 빼내서 합을 구한 뒤 그 다음 카드랑 합치고를 반복해 최종 결과를 낸다.
+   * 근데 내가 푼 방법으로는 계속 틀렸다고 나와서 다른 사람 풀이를 참고했다.
+   * 힙?을 써야 풀 수 있는 것 같았다. 힙에 대한 공부가 필요한 것 같아 리드미에는 incomplete로 표기할 예정
+   */
+  {
+    // 다른 사람 풀이
+    function MinHeap() {
+      this.heap = [0];
+      this.insert = (v) => {
+        this.heap.push(v);
+        let p = this.heap.length - 1;
+        while (p > 1 && this.heap[Math.floor(p / 2)] > this.heap[p]) {
+          let tmp = this.heap[Math.floor(p / 2)];
+          this.heap[Math.floor(p / 2)] = this.heap[p];
+          this.heap[p] = tmp;
+          p = Math.floor(p / 2);
+        }
+      };
+      this.getLength = () => {
+        return this.heap.length;
+      };
+      this.delete = () => {
+        if (this.heap.length - 1 < 1) {
+          return 0;
+        }
+        let deletedItem = this.heap[1];
+        this.heap[1] = this.heap[this.heap.length - 1];
+        this.heap.pop();
+        let p = 1;
+        while (p * 2 < this.heap.length) {
+          let min = this.heap[p * 2];
+          let minP = p * 2;
+          if (p * 2 + 1 < this.heap.length && min > this.heap[p * 2 + 1]) {
+            min = this.heap[p * 2 + 1];
+            minP = p * 2 + 1;
+          }
+          if (this.heap[p] < min) {
+            break;
+          }
+          let tmp = this.heap[p];
+          this.heap[p] = this.heap[minP];
+          this.heap[minP] = tmp;
+          p = minP;
+        }
+        return deletedItem;
+      };
+    }
+
+    const solution = (n, list) => {
+      let cnt = 0;
+
+      for (let i = 1; i < n; i++) {
+        //제일 작은 카드 두개의 묶음을 빼낸다.
+        let card1 = list.delete();
+        let card2 = list.delete();
+        //카드 두 묶음을 계산후 카운트
+        let sum = card1 + card2;
+        cnt += sum;
+        //계산된 결과를 최소힙에 넣어줌
+        list.insert(sum);
+      }
+      console.log(cnt);
+    };
+
+    const readline = require('readline');
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    let input = [];
+    rl.on('line', function (line) {
+      //여러줄 입력
+      input.push(line);
+    }).on('close', function () {
+      let n = Number(input.shift());
+
+      //최소힙 클래스 생성
+      let list = new MinHeap();
+      for (let i = 0; i < n; i++) {
+        //최소힙을 이용하여 값을 하나씩 넣어준다.
+        let tmp = Number(input.shift());
+        list.insert(tmp);
+      }
+      solution(n, list);
+      process.exit();
+    });
+  }
+  {
+    // 내 풀이 - 통과 하지는 못함
+    let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+    let cases = Number(input.shift());
+
+    input = input.map((i) => +i).sort((a, b) => a - b);
+
+    let result = 0;
+
+    for (let i = 1; i < cases; i++) {
+      let first = input.shift();
+      let second = input.shift();
+      let sum = first + second;
+      result += sum;
+      input.unshift(sum);
+    }
+
+    console.log(result);
+  }
+}
