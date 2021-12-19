c1e3263fab035ffe029a94ad65163f8e45ff0688
dkssyddico <dkssyddico@naver.com>
2021-12-19 19:30:37 +0900

    [211219] BOJ complete

--- BAEKJOON/7-string.js
+++ BAEKJOON/7-string.js
@@ -346,3 +346,17 @@
     console.log(dbj.length + '\n' + dbj.sort().join('\n'));
   }
 }
+
+{
+  /**
+   * 1100번 하얀 칸
+   */
+  const board = require('fs').readFileSync('/dev/stdin').toString().split(/\s/);
+  let count = 0;
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      if ((i + j) % 2 === 0 && board[i][j] === 'F') count++;
+    }
+  }
+  console.log(count);
+}
