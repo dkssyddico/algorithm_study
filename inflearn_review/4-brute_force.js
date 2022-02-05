74f8033bbbecfbcd2db9de09df4b6d3fc7834883
dkssyddico <dkssyddico@naver.com>
2022-02-05 23:23:42 +0900

    [220205] inflearn review

--- inflearn_review/4-brute_force.js
+++ inflearn_review/4-brute_force.js
@@ -63,3 +63,46 @@
 
   console.log(answer);
 }
+
+{
+  /**
+   * 멘토링
+   * 멘토 - 멘티가 될 수 있는 모든 경우를 다 만들어본다.
+   * 그리고 모든 시험에서 멘토와 멘티가 등수가 어떻게 되는지 판별한다.
+   * 모든 시험에서 멘토가 멘티의 등수보다 적으면(이래야 시험을 더 잘본거니까) 답에 1을 올려줄 수 있다.
+   */
+  let test = [
+    [3, 4, 1, 2],
+    [4, 3, 2, 1],
+    [3, 1, 4, 2],
+  ];
+
+  let testNum = test.length;
+  let students = test[0].length;
+  let answer = [];
+  // 멘토 멘티가 될 수 있는 경우의 수 구하기
+
+  for (let i = 1; i <= students; i++) {
+    for (let j = 1; j <= students; j++) {
+      let testCount = 0;
+      for (let x = 0; x < testNum; x++) {
+        let rankOne = 0;
+        let rankTwo = 0;
+        for (let y = 0; y < students; y++) {
+          if (test[x][y] === i) {
+            rankOne = y;
+          }
+          if (test[x][y] === j) {
+            rankTwo = y;
+          }
+        }
+        if (rankOne < rankTwo) testCount += 1;
+      }
+      if (testCount === testNum) {
+        answer.push([i, j]);
+      }
+    }
+  }
+
+  console.log(answer);
+}
