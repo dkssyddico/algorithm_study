c56b809fd00fd060afd564ba40b72050aa495b8c
dkssyddico <dkssyddico@naver.com>
2022-03-28 11:12:01 +0900

    [220328] BAEKJOON complete

---
+++ BAEKJOON/Level/510/15666.js
@@ -0,0 +1,37 @@
+/**
+ * N과 M(12)
+ */
+
+const fs = require('fs');
+const input = fs
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n')
+  .map((v) => v.split(' ').map(Number));
+const [N, M] = input[0];
+const nums = input[1].sort((a, b) => a - b);
+
+// let N = 4;
+// let M = 2;
+// let nums = [9, 7, 9, 1].sort((a, b) => a - b);
+
+let result = [];
+let output = [];
+
+function dfs(index, cnt) {
+  if (cnt === M) {
+    result.push(output.join(' '));
+    return;
+  }
+
+  for (let i = index; i < N; i++) {
+    output.push(nums[i]);
+    dfs(i, cnt + 1);
+    output.pop();
+  }
+}
+
+dfs(0, 0);
+
+console.log([...new Set(result)].join('\n'));
