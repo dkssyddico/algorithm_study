3987644e5d70276d8a6b254478cdeff68b7203c6
dkssyddico <dkssyddico@naver.com>
2022-08-20 23:12:31 +0900

    [220820] baekjoon complete

---
+++ BAEKJOON/string/11365.js
@@ -0,0 +1,9 @@
+/**
+ * 11365
+ */
+
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+for (let i = 0; i < input.length - 1; i++) {
+  console.log(input[i].split('').reverse().join(''));
+}
